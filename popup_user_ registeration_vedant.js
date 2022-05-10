ec40fe909c4d84260864476dec4cde36596afac0
UG-099C <103552293+UG-099C@users.noreply.github.com>
2022-05-10 19:12:30 +0930

    Add files via upload

---
+++ popup_user_ registeration_vedant.js
@@ -0,0 +1,15 @@
+function register() {
+    var temp = document.getElementById("navbar");
+    temp.style.backgroundColor = "black";
+    temp.style.opacity = "0.2";
+    var temp = document.getElementById("event");
+    temp.style.color = "black";
+    temp.style.opacity = "0.2";
+    var temp = document.getElementById("footer");
+    temp.style.color = "black";
+    temp.style.opacity = "0.2";
+    var popup = document.getElementById("popup");
+    popup.style.display = "block";
+    popup.style.zIndex = 1000;
+    popup.style.position = "fixed";
+};
